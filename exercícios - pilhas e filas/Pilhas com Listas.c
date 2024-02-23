9daf16ae88009fad69e6187ae8795ad923d83a9d
paulosergioveras <paulosergiovc12@gmail.com>
2024-02-22 22:21:53 -0300

    .

--- exercícios - pilhas e filas/Pilhas com Listas.c
+++ exercícios - pilhas e filas/Pilhas com Listas.c
@@ -2,74 +2,158 @@
 #include <stdlib.h>
 #include <string.h>
 
+//definindo struct de lista encadeada:
+
 typedef struct node{
     int item;
     struct node *next;
 }node;
 
-typedef struct stack{
-    node *top;
+//definindo struct de um nó na pilha:
+
+typedef struct stack_node{
+    node *start_list;
+    struct stack_node *next;
+}stack_node;
+
+//definindo struct da pilha:
+
+typedef struct Stack{
+    stack_node *top;
 }Stack;
 
-void create_stack(Stack *stack){
+//função para criar lista vazia:
+
+node *create_linked_list(){
+    return NULL;
+}
+
+//função para criar stack vazia
+
+Stack *create_stack(){
+    Stack *stack = (Stack*)malloc(sizeof(Stack));
+
+    if(stack == NULL) exit(EXIT_FAILURE);
+
     stack->top = NULL;
+    return stack;
 }
 
-void push(Stack *stack, int item){
+//função para adicionar no final da lista:
+
+node* add_end_list(node *head, int new_item){
     node *new_node = (node*)malloc(sizeof(node));
+    new_node->item = new_item;
+    new_node->next = NULL;
 
-    if(new_node == NULL){
-        exit(EXIT_FAILURE);
+    if (head == NULL) {
+        return new_node;
+    } 
+    else {
+        node *current = head;
+
+        while (current->next != NULL) {
+            current = current->next;
+        }
+
+        current->next = new_node;
+        return head;
     }
-
-    new_node->item = item;
-    new_node->next = stack->top;
-    stack->top = new_node;
 }
 
-void print_stack(){
-    
+//função para adicionar no inicio do nó da stack:
+
+stack_node *add_begin_stack(stack_node *top){
+    stack_node *new_node = (stack_node*)malloc(sizeof(stack_node));
+
+    if(new_node == NULL) exit(EXIT_FAILURE);
+
+    new_node->start_list = create_linked_list();
+    new_node->next = top;
+    return new_node;
 }
 
-void pop(Stack *stack){
-    if(stack->top == NULL){
-        printf("EMPTY STACK\n");
+//função para remover quebra de linha de uma string:
+
+void remove_line_break(char *str){
+    int line_break = strcspn(str, "\n");
+    str[line_break] = '\0';
+}
+
+void print_list(node *head) {
+    if (head == NULL) {
+        printf("\tEMPTY STACK\n");
         return;
     }
 
-    while(stack->top != NULL){
-         node *aux = stack->top;
-        stack->top = stack->top->next;
-        printf("%d\n", aux->item);
-        free(aux);
+    //printf("\t");
+
+    while (head != NULL) {
+        printf("%d", head->item);
+        if (head->next != NULL) {
+            printf(" ");
+        }
+        head = head->next;
     }
-   
+
+    printf("\n");
 }
 
-int main(){
-    Stack stack;
-    create_stack(&stack);
-
-    char cmd[20];
+void scan_stack(Stack *stack){
+    char cmd[5];
+    char aux;
     int item;
 
-    while(scanf("%s", cmd) != EOF){
+    while(fgets(cmd, 5, stdin) != NULL){
+        remove_line_break(cmd);
+
         if(strcmp(cmd, "PUSH") == 0){
-            while(scanf("%d", &item) == 1){
-                push(&stack, item);
+            //se o comando PUSH for dado
+            //inicia um nó no inicio da stack
+
+            stack->top = add_begin_stack(stack->top);
+
+            //vai percorrer o while e colocar os items no final da lista
+            while(scanf("%d%c", &item, &aux) == 2){
+                stack->top->start_list = add_end_list(stack->top->start_list, item);
+
+                if(aux == '\n') break; //se o ultimo caracter for uma quebra de linha, entao encerra
             }
         }
         else if(strcmp(cmd, "POP") == 0){
-            pop(&stack);
-        }
-        else{
-            exit(EXIT_FAILURE);
+            if(stack->top == NULL) printf("EMPTY STACK\n");
+            else{
+                print_list(stack->top->start_list);
+
+                //liberação de memoria:
+
+                stack_node *aux_m = stack->top;
+                stack->top = stack->top->next;
+                free(aux_m);
+            }
+
         }
     }
+}
 
-    while(stack.top != NULL){
-        pop(&stack);
+void free_stack(Stack *stack){
+    while(stack->top != NULL){
+        stack_node *aux = stack->top;
+        stack->top = stack->top->next;
+
+        while(aux->start_list != NULL){
+            node *aux_node = aux->start_list;
+            aux->start_list = aux->start_list->next;
+            free(aux_node);
+        }
+        free(aux);
     }
+    free(stack);
+}
 
+int main(){
+    Stack *stack = create_stack();
+    scan_stack(stack);
+    free_stack(stack);
     return 0;
 }
